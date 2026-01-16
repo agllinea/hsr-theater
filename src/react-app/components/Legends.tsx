1f98e174838297722737018e81af781b445a5150
agllinea <agllinea@gmail.com>
2026-01-16 16:11:38 +1300

    Improve Legends Styling

--- src/react-app/components/Legends.tsx
+++ src/react-app/components/Legends.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import clsx from "clsx";
 import { legends } from "../assets/actors";
 import "./Legends.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const Legends = () => {
   const [currentLegend, setCurrentLegend] = useState<string | null>(null);
@@ -18,8 +19,12 @@ export const Legends = () => {
         {legends.map((legend, index) => (
           <div
             key={index}
-            className={clsx("legend-item", currentLegend === legend.code ? "active" : "")}
-            onClick={() => handleLegendClick(legend.code)}
+            className={clsx(
+              "legend-item",
+              currentLegend === legend.code ? "active" : ""
+            )}
+            onMouseEnter={() => setCurrentLegend(legend.code)}
+            onMouseLeave={()=>setCurrentLegend(null)}
           >
             <span className={clsx("legend-block", legend.code)}></span>
             <span className="legend-code">{legend.code}</span>
@@ -27,17 +32,25 @@ export const Legends = () => {
           </div>
         ))}
       </div>
-      <div className="legend-detail-container">
-        <div
-          className="legend-detail"
-          dangerouslySetInnerHTML={{
-            __html:
-              (currentLegend &&
-                legends.find((l) => l.code === currentLegend)?.description) ??
-              "",
-          }}
-        ></div>
-      </div>
+      <AnimatePresence>
+        {currentLegend && (
+          <motion.div className="legend-detail-container"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3 }}>
+            <div
+              className="legend-detail"
+              dangerouslySetInnerHTML={{
+                __html:
+                  (currentLegend &&
+                    legends.find((l) => l.code === currentLegend)?.description) ??
+                  "",
+              }}
+            ></div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
