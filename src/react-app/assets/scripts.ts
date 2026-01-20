95be0e8d2144b9fb4583a904d94f5173a000560d
agllinea <agllinea@gmail.com>
2026-01-20 16:41:07 +1300

    Upload Scripts

--- src/react-app/assets/scripts.ts
+++ src/react-app/assets/scripts.ts
@@ -10,7 +10,16 @@ export const scripts: Script[] = [
     clips:[]
   },
   {
-    id: "0-02",
+    id: "1-01",
+    chapter: "序幕・空间站「黑塔」",
+    title: "激「冻」人心的大冒险",
+    status: "In Progress",
+    desc: "系统时间23时44分59秒，一个陌生的女人来到空间站「黑塔」……",
+    actors:["kafka","silver-wolf"],
+    clips:[]
+  },
+  {
+    id: "1-02",
     chapter: "序幕・空间站「黑塔」",
     title: "今天是昨天的明天",
     status: "In Progress",
@@ -19,7 +28,7 @@ export const scripts: Script[] = [
     clips:[]
   },
   {
-    id: "0-03",
+    id: "1-03",
     chapter: "序幕・空间站「黑塔」",
     title: "今天是昨天的明天",
     status: "In Progress",
@@ -28,12 +37,30 @@ export const scripts: Script[] = [
     clips:[]
   },
   {
-    id: "0-04",
+    id: "1-04",
     chapter: "序幕・空间站「黑塔」",
     title: "今天是昨天的明天",
     status: "In Progress",
     desc: "系统时间23时44分59秒，一个陌生的女人来到空间站「黑塔」……",
     actors:["kafka","silver-wolf"],
     clips:[]
-  }
+  },
+  {
+    id: "1-05",
+    chapter: "序幕・空间站「黑塔」",
+    title: "今天是昨天的明天",
+    status: "In Progress",
+    desc: "系统时间23时44分59秒，一个陌生的女人来到空间站「黑塔」……",
+    actors:["kafka","silver-wolf"],
+    clips:[]
+  },
+  {
+    id: "1-06",
+    chapter: "序幕・空间站「黑塔」",
+    title: "今天是昨天的明天",
+    status: "In Progress",
+    desc: "系统时间23时44分59秒，一个陌生的女人来到空间站「黑塔」……",
+    actors:["kafka","silver-wolf"],
+    clips:[]
+  },
 ];
